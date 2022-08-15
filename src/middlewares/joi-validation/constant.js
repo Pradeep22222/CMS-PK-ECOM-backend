1e8ec25bf9c0e51236bb4f12458241c716180ff8
PK <pradeepdhital124@gmail.com>
2022-08-15 13:28:43 +1000

    admin login and registration fully finished version 1

--- src/middlewares/joi-validation/constant.js
+++ src/middlewares/joi-validation/constant.js
@@ -1,11 +1,19 @@
 import Joi from "joi";
-export const FNAME = Joi.string().max(20).required();
-export const LNAME = Joi.string().max(20).required();
+export const FNAME = Joi.string().max(20);
+export const LNAME = Joi.string().max(20);
 export const EMAIL = Joi.string().email({ minDomainSegments: 2 });
-export const PASSWORD = Joi.string().max(100).required();
-export const PHONE = Joi.string().max(100).required();
+export const PASSWORD = Joi.string().max(100);
+export const PHONE = Joi.string().max(100);
 export const ADDRESS = Joi.string().max(100).allow("", null);
 export const DATE = Joi.date();
+export const SHORTSTR = Joi.string().max(100);
+export const LONGSTR = Joi.string().max(5000);
 
-export const SHORTSTR = Joi.string().max(50);
-export const LONGSTR = Joi.string().max(500);
+export const validator = (schema, req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    error.status = 200;
+    return next(error);
+  }
+  next();
+};
